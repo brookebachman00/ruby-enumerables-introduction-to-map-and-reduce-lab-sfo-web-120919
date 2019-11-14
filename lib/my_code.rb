8484757cf86cb6f5cf11655a38add59563a2243a
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 15:26:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
+def map_to_negativize(source_array)
+  source_array.map {|i| i* -1}
+end
+def map_to_no_change(source_array)
+  source_array.map {|i|}
+end
+
+def map_to_double(source_array)
+  source_array.map {|i| i * 2}
+end
+
+def map_to_square(source_array)
+  source_array.map {|i| i * i}
+end
+
 # My Code here....
+def reduce_to_total(source_array, starting_point)
+  starting_point.reduce {|i|}
+end
+
+def reduce_to_all_true(source_array)
+  source_array.reduce {|i| if i == }
+end
+
+def reduce_to_any_true(source_array)
+
+end
