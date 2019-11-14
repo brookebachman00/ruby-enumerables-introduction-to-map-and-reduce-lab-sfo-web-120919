b1cea49d762ac0db146fab27492d3eaf392e483b
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 15:48:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,7 @@ end
 def map_to_no_change(source_array)
   index = 0
   new_array = []
-  while index < source_array do
+  while index < source_array.length do
     new_array.push(source_array[index] * 1)
     index += 1
   end
@@ -15,7 +15,7 @@ end
 def map_to_double(source_array)
   index = 0
   new_array = []
-  while index < source_array do
+  while index < source_array.length do
     new_array.push(source_array[index] * 2)
     index += 1
   end
@@ -26,7 +26,7 @@ end
 def map_to_square(source_array)
   index = 0
   new_array = []
-  while index < source_array do
+  while index < source_array.length do
     new_array.push(source_array[index] * index)
     index += 1
   end
@@ -38,7 +38,7 @@ end
 def reduce_to_total(source_array, starting_point)
   total = starting_point
   index = 0
-  while index < source_array do
+  while index < source_array.length do
     total += source_array[index]
     index += 1
   end
