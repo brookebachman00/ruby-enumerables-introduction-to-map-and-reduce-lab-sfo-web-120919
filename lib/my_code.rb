c16be402e16eef858ab25cbfbf8c4151c4b3d60a
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 16:11:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,7 +37,7 @@ end
 
 
 # My Code here....
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   total = starting_point
   index = 0
   while index < source_array.length
