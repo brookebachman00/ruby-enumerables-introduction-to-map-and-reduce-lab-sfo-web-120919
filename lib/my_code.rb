4a00ec7fbadd68c1beada1ae216db65b15c2f68e
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 16:02:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,6 +48,7 @@ def reduce_to_total(source_array, starting_point)
 end
 
 def reduce_to_all_true(source_array)
+  binding.pry
   index = 0
   while index < source_array do
     if source_array[index] == 'false'
@@ -56,7 +57,7 @@ def reduce_to_all_true(source_array)
     index += 1
   end
   return true
-  binding.pry
+  
 end
 
 def reduce_to_any_true(source_array)
