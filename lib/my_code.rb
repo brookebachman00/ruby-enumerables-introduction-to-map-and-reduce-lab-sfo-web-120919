24ae13cea9ed9663fc894ffa79a6b40df176ecf0
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 15:43:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,5 +37,12 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-
+  index = 0
+  while index < source_array do
+    if source_array[index] == 'true'
+      return false
+    end
+    index += 1
+  end
+  return false
 end
